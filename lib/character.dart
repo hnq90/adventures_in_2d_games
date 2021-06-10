ba622ea1785455b1c45befc6daadfbf069edd807
Nick Meinhold <nick.meinhold@gmail.com>
2021-06-10 19:25:37 +1000

    Make Character extnd SpriteAnimationGroupComponent
    - cleaned up our Character class by extending a more appropriate
    flame compnent
    - added an extension to Direction that gives a vector for movement
    - updated input to add a movement effect for every key press

--- lib/character.dart
+++ lib/character.dart
@@ -1,85 +1,51 @@
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/extensions.dart';
-import 'package:flame/game.dart' as game;
 import 'package:flutter/services.dart';
 
 import 'enums/direction.dart';
 import 'input.dart';
 
-typedef StartPos = Map<Direction, double>;
+typedef Position = Vector2;
 
-const StartPos startPosFor = {
-  Direction.up: 192,
-  Direction.down: 0,
-  Direction.left: 96,
-  Direction.right: 288
-};
-
-final Map<Direction, Vector2> vectorFor = {
-  Direction.up: Vector2(0, -1),
-  Direction.down: Vector2(0, 1),
-  Direction.left: Vector2(-1, 0),
-  Direction.right: Vector2(1, 0)
-};
-
-class Character {
-  // Private constructor.
-  Character._(Map<Direction, SpriteAnimation> animationFor,
-      {Vector2? position, Vector2? size})
-      : _currentPosition = position ?? Vector2(240, 50),
-        _size = size ?? Vector2(60, 60) {
-    _area = _currentPosition & _size;
-    _animationFor = animationFor;
-    _currentAnimation = animationFor[_currentDirection]!;
-  }
-
-  ////////////////// start static section ///////////////////////
+class Character extends SpriteAnimationGroupComponent<Direction> {
+  // Private constructor - the async create method is how an object is created.
+  Character._(Map<Direction, SpriteAnimation> animations,
+      {required Position start})
+      : super(
+            size: Vector2(64, 64),
+            position: start,
+            animations: animations,
+            current: Direction.down);
 
   // Static async create method so we can load sprite animations.
   static Future<Character> create(String path) async {
-    final mapOfAnimations = <Direction, SpriteAnimation>{};
+    final animations = <Direction, SpriteAnimation>{};
+
+    // The x position of each sprite in the sprite sheet.
+    const spriteX = <double>[192, 0, 96, 288];
 
     for (var direction in Direction.values) {
-      mapOfAnimations[direction] = await SpriteAnimation.load(
+      animations[direction] = await SpriteAnimation.load(
           path,
           SpriteAnimationData.sequenced(
               amount: 3,
               textureSize: Vector2(32, 32),
               stepTime: 0.1,
-              texturePosition: Vector2(startPosFor[direction]!, 0)));
+              texturePosition: Vector2(spriteX[direction.index], 0)));
     }
 
-    return Character._(mapOfAnimations);
+    return Character._(animations, start: Position(240, 50));
   }
-
-  ////////////////// end static section ///////////////////////
 
   void changeDirection(RawKeyEvent event) {
     if (event is RawKeyDownEvent) {
-      final eventDirection = Input.directionFrom(event);
-      if (eventDirection != null) {
-        _currentDirection = eventDirection;
-        _currentAnimation = _animationFor[_currentDirection]!;
-      }
+      current = Input.directionFrom(event);
+      final newPoint = position + current.vector;
+      addEffect(MoveEffect(path: [newPoint], speed: 200.0));
     }
   }
 
-  void update(double dt) {
-    _currentAnimation.update(dt);
-    final positionDelta = vectorFor[_currentDirection]! * 0.5;
-    _currentPosition += positionDelta;
-  }
-
-  void render(Canvas canvas) {
-    _currentAnimation
-        .getSprite()
-        .render(canvas, position: _currentPosition, size: _size);
-  }
-
-  Vector2 _currentPosition;
-  final Vector2 _size;
-  late final Rect _area;
-  Direction _currentDirection = Direction.down;
-  late SpriteAnimation _currentAnimation;
-  late final Map<Direction, SpriteAnimation> _animationFor;
+  void update(double dt) => super.update(dt);
+  void render(Canvas canvas) => super.render(canvas);
 }
--- lib/enums/direction.dart
+++ lib/enums/direction.dart
@@ -1,1 +1,14 @@
+import 'package:flame/extensions.dart';
+
 enum Direction { up, down, left, right }
+
+final _movementIn = <Direction, Vector2>{
+  Direction.up: Vector2(0, -64),
+  Direction.down: Vector2(0, 64),
+  Direction.left: Vector2(-64, 0),
+  Direction.right: Vector2(64, 0)
+};
+
+extension DirectionExtension on Direction? {
+  Vector2 get vector => _movementIn[this] ?? Vector2(0, 0);
+}
